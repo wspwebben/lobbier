fe88de0d812b89494af02318751dbb674c197672
webben <wspwebben@gmail.com>
2020-05-13 01:06:51 +0700

    update public assets

--- src/server.js
+++ src/server.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.static(path.join(__dirname, '/public')))
 
 app.get('/', (req, res) => {
-  res.sendFile(`${__dirname}/index.html`);
+  res.sendFile(`${__dirname}/public/index.html`);
 })
 
 const PORT = 3000
