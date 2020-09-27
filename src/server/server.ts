99c181699885f9866a2ea2864658d3f28dd5883e
wspwebben <wspwebben@gmail.com>
2020-09-28 01:14:36 +0700

    add client files

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,13 +1,15 @@
 import { createServer } from 'http';
 import * as express from 'express';
+import * as path from 'path';
 
 const app = express();
 const port = process.env.port || 3000;
 
 const server = createServer(app);
 
-app.get("/", (req: any, res: any) => {
-  res.send("hello world");
+app.use(express.static(path.resolve('./src/client')))
+app.get('/', (req: express.Request, res: express.Response) => {
+  res.sendFile(path.resolve('./src/client/index.html'));
 });
 
 server.listen(port, () => {
