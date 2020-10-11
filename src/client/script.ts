31de9fb074ee44b80d388d67a04f6a0cb9e86311
wspwebben <wspwebben@gmail.com>
2020-10-12 01:57:09 +0700

    add basic messaging

--- src/client/script.ts
+++ src/client/script.ts
@@ -1,13 +1,32 @@
 import * as io from 'socket.io-client';
 
-import { SERVER_URL } from '../common/consts';
+import { SERVER_URL, EVENTS } from '../common/consts';
 
 const socket = io(SERVER_URL);
 
-socket.on('message', (message: any) => {
-  console.log(message);
-})
+function createRoom() {
+  socket.emit(EVENTS.CREATE_ROOM, {
 
-function sendMsg(message = 'hello world') {
-  socket.emit("message", message);
+  });
 }
+
+function joinRoom(id: number = 1) {
+  socket.emit(EVENTS.JOIN_ROOM, {
+    id
+  })
+}
+
+const form = document.querySelector('form');
+const roomIdInput = <HTMLInputElement>form.querySelector('.input');
+
+form.addEventListener('submit', (event: Event) => {
+  event.preventDefault();
+
+  const roomId = Number(roomIdInput.value);
+
+  if (roomId) {
+    joinRoom(roomId);
+  } else {
+    createRoom();
+  }
+})
--- src/server/server.ts
+++ src/server/server.ts
@@ -3,6 +3,8 @@ import * as express from 'express';
 import * as socketio from "socket.io";
 import * as path from 'path';
 
+import { EVENTS } from '../common/consts';
+
 const app = express();
 const port = process.env.port || 3000;
 
@@ -13,15 +15,18 @@ app.get('/', (req: express.Request, res: express.Response) => {
   res.sendFile(path.resolve('./dist/index.html'));
 });
 
-const io = socketio(server);
+const socket = socketio(server);
 
-io.on('connection', (socket: any) => {
+socket.on('connection', (client: any) => {
   console.log('connected');
 
-  socket.on('message', (message: any) => {
-    console.log(message);
-    socket.emit('message', `response to ${message}`);
-  });
+  client.on(EVENTS.CREATE_ROOM, (message: any) => {
+    console.log('Created', message);
+  })
+
+  client.on(EVENTS.JOIN_ROOM, (room: any) => {
+    console.log('Joined', room);
+  })
 });
 
 server.listen(port, () => {
